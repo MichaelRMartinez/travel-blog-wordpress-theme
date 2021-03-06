52ba3620e88876d7b2f1c25a08ff7e1c58e080ad
michaelrmartinez <mirima92@gmail.com>
2021-03-06 15:53:22 -0500

    set up base project files; trimmed content.php and single.php to base functionality

---
+++ template-parts/content.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Template part for displaying posts
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
+ *
+ * @package nomadsun
+ */
+
+?>
+
+<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+	<header class="entry-header">
+		
+	</header><!-- .entry-header -->
+
+	<div class="entry-content">
+		
+	</div><!-- .entry-content -->
+
+	<footer class="entry-footer">
+		<?php // nomadsun_entry_footer(); ?>
+	</footer><!-- .entry-footer -->
+</article><!-- #post-<?php the_ID(); ?> -->
