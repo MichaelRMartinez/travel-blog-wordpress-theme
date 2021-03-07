9140e67658a2e49126d01c71c559054ee94d4f3b
michaelrmartinez <mirima92@gmail.com>
2021-03-06 19:12:16 -0500

    added Google Fonts and created font classes

--- template-parts/content.php
+++ template-parts/content.php
@@ -16,14 +16,14 @@
 	<?php endif; ?>">
 		<div class="hero-content white tc">
 			<!-- title -->
-			<h1 class="bold mt0 mb2 ttu hero-heading"><?php the_title(); ?></h1>
+			<h1 class="bold mt0 mb2 ttu hero-heading archivo"><?php the_title(); ?></h1>
 			<!-- subheading -->
 			<?php if( get_field('subhead') ): ?>
-				<p class="mb6 mt0 white ttu hero-subhead"><?php the_field('subhead'); ?></p>
+				<p class="mb6 mt0 white ttu hero-subhead tenor"><?php the_field('subhead'); ?></p>
 			<?php endif; ?>
 			<!-- formatted date -->
 			<?php if( get_field('date') ): ?>
-				<p class="f6 bold ma0 white ttu tracked date"><?php the_field('date'); ?></p>
+				<p class="f6 bold ma0 white ttu tracked date archivo"><?php the_field('date'); ?></p>
 			<?php endif; ?>
 		</div>
 	</section>
